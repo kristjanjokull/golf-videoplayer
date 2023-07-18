bd33f4b2a22e1e2e5a74075b27bde4addc98dba4
Kristján Jökull Sigurðsson <kristjanjokull@gmail.com>
2023-07-18 15:11:45 +0000

    rangeslider update

--- src/components/Rangeslider/Rangeslider.tsx
+++ src/components/Rangeslider/Rangeslider.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import ReactSlider, { ReactSliderProps } from "react-slider";
 import { horizontalSlider, track, thumb, activeTrack } from "./Rangeslider.css";
+import { useVideoStore } from "../../stateManager/store";
 
 type RenderThumb = ReactSliderProps<number>["renderThumb"];
 type RenderTrack = ReactSliderProps<number>["renderTrack"];
@@ -19,13 +20,24 @@ const Thumb: RenderThumb = (props, state) => {
 };
 
 export const RangeSlider = () => {
+  const videoRef = useVideoStore((state) => state.videoRef);
   const [value, setValue] = useState(0);
+
+  const handleChange = (v: number) => {
+    setValue(v);
+
+    if (videoRef.current) {
+      const videoDuration = videoRef.current.duration;
+      const newTime = (v / 100) * videoDuration;
+      videoRef.current.currentTime = newTime;
+    }
+  };
 
   return (
     <ReactSlider
       className={horizontalSlider}
       value={value}
-      onChange={(v) => setValue(v)}
+      onChange={handleChange}
       renderTrack={Track}
       renderThumb={Thumb}
     />
