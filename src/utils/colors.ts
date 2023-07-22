1bd8187a74f3f7a71a543a3823e35f80044484c6
Kristján Jökull Sigurðsson <kristjanjokull@gmail.com>
2023-07-22 20:33:31 +0000

    colors variables init

---
+++ src/utils/colors.ts
@@ -0,0 +1,36 @@
+export const colors = {
+  light: {
+    background: "#ece2e2",
+    buttons: {
+      idle: "#ece2e2",
+      hover: "#dbdcdf",
+    },
+    controlButtons: {
+      idle: "white",
+      hover: "black",
+    },
+    dock: "rgba(117, 117, 117, 0.5)",
+    slider: {
+      knob: "#ecc90a",
+      trackPlayed: "#ecc90a",
+      trackUnPlayed: "#d7d5d5",
+    },
+  },
+  dark: {
+    background: "#ece2e2",
+    buttons: {
+      idle: "#ece2e2",
+      hover: "#dbdcdf",
+    },
+    controlButtons: {
+      idle: "white",
+      hover: "black",
+    },
+    dock: "rgba(117, 117, 117, 0.5)",
+    slider: {
+      knob: "#ecc90a",
+      trackPlayed: "#ecc90a",
+      trackUnPlayed: "#d7d5d5",
+    },
+  },
+};
