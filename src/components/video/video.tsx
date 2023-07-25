46ab6362822e89c1d20ac67640e6f9918a05a0b9
Kristján Jökull Sigurðsson <kristjanjokull@gmail.com>
2023-07-25 14:55:04 +0000

    video css file fix

--- src/components/video/video.tsx
+++ src/components/video/video.tsx
@@ -7,7 +7,7 @@ import { ThemeSwitch } from "$components/themeSwitch/themeSwitch";
 import { VideoFlip } from "$components/videoFlip/videoFlip";
 import { useVideoStore } from "$utils/state";
 
-import { dock, video, videoContainer, videoFlipped } from "./videoTemp.css";
+import { dock, video, videoContainer, videoFlipped } from "./video.css";
 
 type Props = {
   src: string;
