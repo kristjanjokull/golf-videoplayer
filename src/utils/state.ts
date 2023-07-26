a3533ba83b74044c26804fcb5d3ffb25d50a5408
Kristján Jökull Sigurðsson <kristjanjokull@gmail.com>
2023-07-26 08:51:22 +0000

    setTheme added to ThemeStore

--- src/utils/state.ts
+++ src/utils/state.ts
@@ -3,15 +3,17 @@ import { create } from "zustand";
 
 // =============== Theme store ====================
 
-type Theme = "light" | "dark";
+export type Theme = "light" | "dark";
 
 interface ThemeStore {
   theme: Theme;
+  setTheme: (theme: Theme) => void;
   toggleTheme: () => void;
 }
 
 export const useThemeStore = create<ThemeStore>((set) => ({
   theme: "light",
+  setTheme: (theme: Theme) => set({ theme: theme }),
   toggleTheme: () => {
     set((state) => ({
       theme: state.theme === "light" ? "dark" : "light",
