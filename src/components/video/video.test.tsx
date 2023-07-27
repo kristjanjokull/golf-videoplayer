67381166d5542fde0841c2aec66f3b9063afc108
Kristján Jökull Sigurðsson <kristjanjokull@gmail.com>
2023-07-27 11:42:13 +0000

    test update

--- src/components/video/video.test.tsx
+++ src/components/video/video.test.tsx
@@ -1,13 +1,14 @@
-// import { render } from "@testing-library/react";
-import { describe, test } from "vitest";
+import { render } from "@testing-library/react";
+import { describe, expect, test } from "vitest";
 
-// import videoSrc from "$assets/demoswing.mp4";
+import videoSrc from "$assets/demoswing.mp4";
 
-// import { Video } from "./video";
+import { Video } from "./video";
 
 describe("Video", () => {
   test("renders without crashing", () => {
-    // const { container } = render(<Video src={videoSrc} />);
-    // expect(container).toBeInTheDocument(); // Cast container to HTMLElement
+    const { container } = render(<Video src={videoSrc} />);
+
+    expect(container).toBeDefined();
   });
 });
