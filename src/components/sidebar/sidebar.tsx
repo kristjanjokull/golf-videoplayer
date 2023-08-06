2361a39534dc7dc67b8967399b3bdd1ba27ceeb9
Kristján Jökull Sigurðsson <kristjanjokull@gmail.com>
2023-08-06 13:53:40 +0000

    file fix

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -1,6 +1,6 @@
 import React, { FunctionComponent } from "react";
 
-import { sidebar } from "./sidebarTemp.css";
+import { sidebar } from "./sidebar.css";
 
 interface SidebarProps {
   components: FunctionComponent[];
