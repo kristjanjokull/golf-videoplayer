29a37d5ecfc9bce9345b46aabb03138fc2834d5f
Kristján Jökull Sigurðsson <kristjanjokull@gmail.com>
2023-08-08 21:20:38 +0000

    update

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -1,14 +1,12 @@
 import React, { FunctionComponent } from "react";
 
-import { sidebar } from "./sidebar.css";
-
 interface SidebarProps {
   components: FunctionComponent[];
 }
 
 export const Sidebar: React.FC<SidebarProps> = ({ components }) => {
   return (
-    <div className={sidebar}>
+    <div className="sidebar">
       {components.map((Component, index) => (
         <Component key={index} />
       ))}
