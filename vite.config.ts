e66882e197e89ed0ccb5ffd0f0d7c5ade385b6a7
Kristján Jökull Sigurðsson <kristjanjokull@gmail.com>
2023-08-09 12:22:05 +0000

    vanilla extract removed

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,13 @@
 // import { defineConfig } from "vite";
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
-import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 import path from "path";
 
 // https://dev.to/avxkim/setup-path-aliases-w-react-vite-ts-poa
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      $components: path.resolve(__dirname, "./src/components"),
-      $utils: path.resolve(__dirname, "./src/utils"),
-    },
-  },
-  plugins: [react(), vanillaExtractPlugin()],
+  plugins: [react()],
   test: {
     globals: true,
     environment: "jsdom",
